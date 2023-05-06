4c3cbc31194b5a57e9d3d3a302d4b797c78ef5ad
Serhii Vovchenko <seriivigas@gmail.com>
2023-05-06 16:52:45 +0300

    Todo-List

---
+++ src/Pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className="notification is-danger is-light">
+      Oops... It seems you are lost. Page not found.
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import 'bulma/css/bulma.css';
+import './styles/index.scss';
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
+import { TodoPage } from './Pages/TodoPage';
+import { PageRouters } from './types/PageRouters';
+import { NotFoundPage } from './Pages/NotFoundPage';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <Routes>
+      <Route path={`${PageRouters.Home}`} element={<App />}>
+        <Route index element={<TodoPage />} />
+        <Route path={`${PageRouters.All}`} element={<TodoPage />} />
+        <Route path={`${PageRouters.Active}`} element={<TodoPage />} />
+        <Route path={`${PageRouters.Completed}`} element={<TodoPage />} />
+      </Route>
+
+      <Route path={`${PageRouters.NotFound}`} element={<NotFoundPage />} />
+    </Routes>
+  </HashRouter>,
   document.getElementById('root'),
 );
